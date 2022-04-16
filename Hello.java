ac3bccb09192972ce2a6a1147f361529f63cc83f
Sathishkumar1287 <101247091+Sathishkumar1287@users.noreply.github.com>
2022-04-17 01:25:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
 
 	public static void main(String[] args) {
 	System.out.println("Hello");
-
+	System.out.println("Sathish");//New comment
+	System.out.println("Welcome!");//New comment
 	}
 
 }
